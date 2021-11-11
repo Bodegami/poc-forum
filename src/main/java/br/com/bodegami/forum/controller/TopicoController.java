6d23853ec23fe8f2eb2cdba56ee9cd2ad8b22d09
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2021-11-10 22:59:31 -0300

    adicionado TopicoDto

--- src/main/java/br/com/bodegami/forum/controller/TopicoController.java
+++ src/main/java/br/com/bodegami/forum/controller/TopicoController.java
@@ -3,22 +3,22 @@ package br.com.bodegami.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.bodegami.forum.controller.dto.TopicoDto;
 
-@Controller
+@RestController
 public class TopicoController {
 
 	@ResponseBody
 	@RequestMapping("/topicos")
-	public List<Topico> lista() {
-		Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação"));
-		
-		return Arrays.asList(topico, topico, topico);	
+	public List<TopicoDto> lista() {
+		Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
+		return TopicoDto.converter(Arrays.asList(topico, topico, topico));	
 	}
 	
 }
