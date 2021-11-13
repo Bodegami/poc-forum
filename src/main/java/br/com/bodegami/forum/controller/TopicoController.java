383ab788c068f609a687398bef13689804c2b2ce
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2021-11-13 20:57:55 -0300

    adicionado EnableSpringDataWebSupport e refatorado paginacao

--- src/main/java/br/com/bodegami/forum/controller/TopicoController.java
+++ src/main/java/br/com/bodegami/forum/controller/TopicoController.java
@@ -7,9 +7,9 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -47,9 +47,7 @@ public class TopicoController {
 	@ResponseBody
 	@GetMapping
 	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, 
-			@RequestParam int pagina, @RequestParam int qtd, @RequestParam String ordenacao) {
-
-		Pageable paginacao = PageRequest.of(pagina, qtd, Direction.DESC, ordenacao);
+			@PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 10) Pageable paginacao) {
 
 		if (nomeCurso == null) {
 			Page<Topico> lista = topicoRepository.findAll(paginacao);
