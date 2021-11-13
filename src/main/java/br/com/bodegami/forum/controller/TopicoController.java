bb5adc6d10245873274d778249d054fadcaa27a4
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2021-11-13 20:40:58 -0300

    adicionando paginacao no metodo Get topicos

--- src/main/java/br/com/bodegami/forum/controller/TopicoController.java
+++ src/main/java/br/com/bodegami/forum/controller/TopicoController.java
@@ -1,12 +1,14 @@
 package br.com.bodegami.forum.controller;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -42,12 +45,15 @@ public class TopicoController {
 
 	@ResponseBody
 	@GetMapping
-	public List<TopicoDto> lista(String nomeCurso) {
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, Integer pagina, Integer qtd) {
+
+		Pageable paginacao = PageRequest.of(pagina, qtd);
+
 		if (nomeCurso == null) {
-			List<Topico> lista = topicoRepository.findAll();
+			Page<Topico> lista = topicoRepository.findAll(paginacao);
 			return TopicoDto.converter(lista);
 		} else {
-			List<Topico> lista = topicoRepository.carregarPorNomeDoCurso(nomeCurso);
+			Page<Topico> lista = topicoRepository.findByCursoNome(nomeCurso, paginacao);
 			return TopicoDto.converter(lista);
 		}
 
