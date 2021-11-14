446c571b75e253e06a0ae0ac7f6f479206bf3ca5
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2021-11-14 19:24:23 -0300

    adicionado dependency spring-boot-starter-cache e EnableCaching

--- src/main/java/br/com/bodegami/forum/controller/TopicoController.java
+++ src/main/java/br/com/bodegami/forum/controller/TopicoController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
@@ -46,6 +47,7 @@ public class TopicoController {
 
 	@ResponseBody
 	@GetMapping
+	@Cacheable(value = "listaDeTopicos")
 	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, 
 			@PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 10) Pageable paginacao) {
 
