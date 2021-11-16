025ec0eb0ccd12bf7df1daa73fb8d7b60b8eda45
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2021-11-15 21:54:52 -0300

    adicionado TokenService, metodo gerarToken e logica de criptografia

--- src/main/java/br/com/bodegami/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/bodegami/forum/config/security/SecurityConfigurations.java
@@ -1,7 +1,9 @@
 package br.com.bodegami.forum.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -15,6 +17,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
 	private final AutenticacaoService autenticacaoService;
+	
+	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
 
 	public SecurityConfigurations(AutenticacaoService autenticacaoService) {
 		this.autenticacaoService = autenticacaoService;
