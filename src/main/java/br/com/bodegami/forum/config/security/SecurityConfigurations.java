a31a643ef9150b4dad4bba94b4320850ea1852b2
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2021-11-15 22:53:52 -0300

    adicionado AutenticacaoViaTokenFilter e metodo recuperarToken

--- src/main/java/br/com/bodegami/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/bodegami/forum/config/security/SecurityConfigurations.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -44,7 +45,8 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 		.anyRequest().authenticated()
 		//.and().formLogin()
 		.and().csrf().disable()
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and().addFilterBefore(new AutenticacaoViaTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 
 	// Configuracoes de recursos estaticos(js, css, imagens, etc.)
